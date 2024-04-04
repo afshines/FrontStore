b5ef164d7d0022468589c541ea340ec1e61952ac
Esmail poryosef <afrangart@gmail.com>
2024-04-04 15:04:16 +0330

    first commit

--- src/data/config.tsx
+++ src/data/config.tsx
@@ -1,5 +1,5 @@
-export let url=  'http://localhost:8000';
-//export let url=  'http://api.taminpaidar.com';
+//export let url=  'http://localhost:8000';
+export let url=  'http://api.taminpaidar.com';
 
 //export let url ='http://localhost:20032';
 // export let novinpay = 'C77E145B-CFB6-45F6-A2E7-9DE24899AF06'
