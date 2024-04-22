1040b08cc0e090ac59dbd1e51e1798f34236c439
esmail <afrangart@gmail.com>
2024-04-22 15:39:31 +0330

    fix red

--- src/components/certificate.tsx
+++ src/components/certificate.tsx
@@ -5,7 +5,7 @@ export default function Certificate(){
                 <img className="  " src="/images/image 20.png"   height={100}/>
               </div> */}
               <div className="w-[81px] h-[80px] mt-10 z-40  ml-20">
-              <embed src="/namad.html" width={80} style={{backgroundColor:'red'}} height={80} ></embed>
+              <embed src="/namad.html" width={80}  height={80} ></embed>
 
               </div>
 
